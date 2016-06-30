ec5db1287e10127fe397ba990a9e0806b968a5ca
Will Wright <will.wright@solesociety.com>
2016-06-30 15:57:33 -0700

    updating hosts

--- filebeats/start.sh
+++ filebeats/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -XPUT 'http://172.31.13.57:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
+curl -XPUT 'http://elk:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
 /etc/init.d/filebeat start
