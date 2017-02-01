fe4bf44012453916a1ccb1f95e407a16d2085fba
Will Wright <wwrig@guidance.com>
2017-02-01 10:00:09 -0800

    elk and filebeats updates

--- filebeats/start.sh
+++ filebeats/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl -XPUT 'http://elk:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
-/etc/init.d/filebeat start
+/usr/share/filebeat/bin/filebeat -e -v -c /etc/filebeat/filebeat.yml
